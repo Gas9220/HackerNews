c0a60f8686554ed2bb27b97fb87bfb5c74e2a2a3
Gaspare <Gas9220@users.noreply.github.com>
2024-03-19 16:14:38 +0100

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Gaspare Monte on 19/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Gaspare Monte on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
